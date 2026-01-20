e5b967f89786c578b1ddfa19b91a50159e0095cc
Yerzhanat Toleubay <ms.erzhanat@gmail.com>
2026-01-20 18:12:43 +0500

    fav

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
       <CompletedProjects/>
       <EquipmentAppleDock/>
       <TestimonialsSlider/>
-      <ClientsMarquee/>
+      {/* <ClientsMarquee/> */}
       <CertificatesSlider/>
     </>
   );
